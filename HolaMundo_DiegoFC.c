33fcf2ea7c364a8b37020b9048c76e56ec71a29b
DiegoLaGoat <al2203031129@azc.uam.mx>
2021-08-17 18:07:57 -0500

    Primer programa (Hola mundo)
    Configuración del compilador y carpeta bin.

---
+++ HolaMundo_DiegoFC.c
@@ -0,0 +1,11 @@
+/*
+    Actividad: Impresión de un mensaje en pantalla.
+    Autor: Diego Flores Chávez.
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf(" %c Hola, mi nombre es Diego Flores Ch%cvez %c", 174, 160, 175);
+    return 0;
+}
